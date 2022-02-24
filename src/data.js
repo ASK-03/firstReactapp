bda768f3958ffac7426b745ae3261788ffc8b093
ASK-03 <abhisheksinghkushwaha22@gmail.com>
2022-02-24 12:41:12 +0530

    updated Norway to NORWAY

--- src/data.js
+++ src/data.js
@@ -18,7 +18,7 @@ export default [
     {
         id: 3,
         image: "Geirangerfjord.jfif",
-        country: "Norway",
+        country: "NORWAY",
         place: "Geirangerfjord",
         date: "1 Oct, 2021 - 18 Oct, 2021",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique nostrum perspiciatis nam eaque illum tempora, distinctio minima placeat laudantium quos laboriosam iure inventore optio quam provident earum voluptate est cupiditate fugiat magnam explicabo. Omnis aliquam eaque, quae modi voluptas molestias placeat unde nulla magnam eos dolore voluptatem laborum dignissimos. Deserunt."
